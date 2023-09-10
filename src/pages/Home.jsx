8ed8758ab37b597c9fa03a85161c994025d6be87
Gaurav panwar <gp771234@gmail.com>
2023-09-10 15:12:05 +0530

    a;ldfkj

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,19 +4,18 @@ import wdimg from "../assets/web_developer.png";
 import prgimg from "../assets/programmer.png";
 import auto_app from "../assets/auto_applica.png";
 import videotronics from "../assets/videotronics.png";
-import collegeLogo from "../assets/college_logo.png"
-import lotusLogo from "../assets/lotus_valley_logo.png"
-import springdalesLogo from "../assets/springdales_school.png"
-import rocketsend from "../assets/rocket.png"
-import chess from "../assets/chess.png"
-import gamer from "../assets/gamer.png"
+import collegeLogo from "../assets/college_logo.png";
+import lotusLogo from "../assets/lotus_valley_logo.png";
+import springdalesLogo from "../assets/springdales_school.png";
+import rocketsend from "../assets/rocket.png";
+import chess from "../assets/chess.png";
+import gamer from "../assets/gamer.png";
 import { useEffect, useRef } from "react";
-import emailjs from "@emailjs/browser"
-import spicy_algo from "../assets/spicy_algo.png"
+import emailjs from "@emailjs/browser";
+import spicy_algo from "../assets/spicy_algo.png";
 
 const Home = () => {
-
-  document.title = 'Gaurav Panwar';
+  document.title = "Gaurav Panwar";
 
   useEffect(() => {
     let card = document.getElementsByClassName("cardhover");
@@ -48,13 +47,22 @@ const Home = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_2sv22fi', 'template_847g5y5', form.current, 'CevKviUGJtabB68KT')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_2sv22fi",
+        "template_847g5y5",
+        form.current,
+        "CevKviUGJtabB68KT"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-          alert('Message Sent!')
-      }, (error) => {
+          alert("Message Sent!");
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
   };
 
   return (
@@ -74,7 +82,12 @@ const Home = () => {
         <h3>INTRODUCTION</h3>
         <h1>OVERVIEW</h1>
         <p>
-        Tech enthusiast at 21, specializing in web development. Proficient in HTML, CSS, and JavaScript, I create user-friendly and visually appealing websites. A meticulous and creative problem solver, I thrive in collaborative settings. Committed to staying current with technology trends, my portfolio reflects my dedication to impactful web solutions.
+          Tech enthusiast at 21, specializing in web development. Proficient in
+          HTML, CSS, and JavaScript, I create user-friendly and visually
+          appealing websites. A meticulous and creative problem solver, I thrive
+          in collaborative settings. Committed to staying current with
+          technology trends, my portfolio reflects my dedication to impactful
+          web solutions.
         </p>
         <div className="cards">
           <SkillCard src={wdimg} title="Web Developer" />
@@ -84,11 +97,22 @@ const Home = () => {
         </div>
       </div>
       <div className="findMe">
-        <h1><span className="blue">Find Me Online</span></h1>
+        <h1>
+          <span className="blue">Find Me Online</span>
+        </h1>
         <div className="links">
-          <a href="https://github.com/TheToadSannin" target="_blank"><i class="fa-brands fa-github" style={{"color": "#FFFFFF"}}></i></a>
-          <a href="https://twitter.com/TheToadSannin" target="_blank"><i class="fa-brands fa-square-twitter" style={{"color": "#FFFFFF"}}></i></a>
-          <a href="www.linkedin.com/in/gaurav-panwar-85822a22b" target="_blank"><i class="fa-brands fa-linkedin" style={{"color": "#FFFFFF"}}></i></a>
+          <a href="https://github.com/TheToadSannin" target="_blank">
+            <i class="fa-brands fa-github" style={{ color: "#FFFFFF" }}></i>
+          </a>
+          <a href="https://twitter.com/TheToadSannin" target="_blank">
+            <i
+              class="fa-brands fa-square-twitter"
+              style={{ color: "#FFFFFF" }}
+            ></i>
+          </a>
+          <a href="www.linkedin.com/in/gaurav-panwar-85822a22b" target="_blank">
+            <i class="fa-brands fa-linkedin" style={{ color: "#FFFFFF" }}></i>
+          </a>
         </div>
       </div>
       <div className="projects" id="projects">
@@ -107,27 +131,34 @@ const Home = () => {
         </div>
         <div className="project-cards">
           <ProjectCard
-            link={"https://good-teal-woodpecker-tam.cyclic.app"}
+            link={"https://auto-applica.vercel.app/"}
             img={auto_app}
             name={"Auto-Applica"}
-            detail = {"Designed and developed a secure web application empowering students to effortlessly generate, submit, and communicate with teachers via encrypted channels, enhancing academic workflows and data privacy in education."}
+            detail={
+              "Designed and developed a secure web application empowering students to effortlessly generate, submit, and communicate with teachers via encrypted channels, enhancing academic workflows and data privacy in education."
+            }
           />
           <ProjectCard
             link={"https://www.livevideotronics.com/"}
             img={videotronics}
             name={"Video-Tronics"}
-            detail = {"Crafted a specialized web application for a cutting-edge drone and camera repair center, streamlining repair requests, inventory management, and secure client communication. Elevating efficiency and customer satisfaction in the tech repair industry."}
+            detail={
+              "Crafted a specialized web application for a cutting-edge drone and camera repair center, streamlining repair requests, inventory management, and secure client communication. Elevating efficiency and customer satisfaction in the tech repair industry."
+            }
           />
           <ProjectCard
             link={"https://spicy-algorithm.vercel.app/"}
             img={spicy_algo}
             name={"Spicy-Algorithm"}
-            detail={"Crafted a user-centric web app for cafeteria feedback, bridging students and culinary services. Enhancing satisfaction and driving culinary service improvements through streamlined feedback collection."}
+            detail={
+              "Crafted a user-centric web app for cafeteria feedback, bridging students and culinary services. Enhancing satisfaction and driving culinary service improvements through streamlined feedback collection."
+            }
           />
         </div>
         <div>
           <h1>
-            Visit My <a href="https://github.com/TheToadSannin/">Github</a> For More 
+            Visit My <a href="https://github.com/TheToadSannin/">Github</a> For
+            More
           </h1>
         </div>
       </div>
@@ -135,11 +166,23 @@ const Home = () => {
         <h1>
           <span className="blue">Qualifications</span>
         </h1>
-      <p>All of my Qualifications till now</p>
+        <p>All of my Qualifications till now</p>
         <div className="quali-cards">
-          <QualiCard type="College / Unviersity" image = {collegeLogo} detail="Pursuing Bachelor Of Technology in Computer Science and Engineering(2020 - 2024) from Amity University Madhya Pradesh Gwalior . With a major in Artificial Intelligence and Machine Learning. Holding the aggregate CGPA of 7.55."/>
-          <QualiCard type="Higher secondary" image = {springdalesLogo} detail="Completed Higher Secondary schooling from Springdales School from Kota Rajasthan with a major in Science and Mathematics with the aggregate percentage of 67%."/>
-          <QualiCard type="High School" image = {lotusLogo} detail="Completed High Schooling from Lotus Valley School in Mandsaur Mahdya Pradesh. with the Aggregate of 72%. "/>
+          <QualiCard
+            type="College / Unviersity"
+            image={collegeLogo}
+            detail="Pursuing Bachelor Of Technology in Computer Science and Engineering(2020 - 2024) from Amity University Madhya Pradesh Gwalior . With a major in Artificial Intelligence and Machine Learning. Holding the aggregate CGPA of 7.55."
+          />
+          <QualiCard
+            type="Higher secondary"
+            image={springdalesLogo}
+            detail="Completed Higher Secondary schooling from Springdales School from Kota Rajasthan with a major in Science and Mathematics with the aggregate percentage of 67%."
+          />
+          <QualiCard
+            type="High School"
+            image={lotusLogo}
+            detail="Completed High Schooling from Lotus Valley School in Mandsaur Mahdya Pradesh. with the Aggregate of 72%. "
+          />
         </div>
       </div>
       <div className="contactus" id="contactus">
@@ -147,12 +190,31 @@ const Home = () => {
           <div id="earth"></div>
         </div>
         <div className="inputs">
-          <h1><span className="blue">CONTACT ME HERE</span></h1>
+          <h1>
+            <span className="blue">CONTACT ME HERE</span>
+          </h1>
           <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name = "user_email" placeholder="Your Email" required />
-          <input type="text" name = "user_name" placeholder="Your Name" required />
-          <textarea type="text" name="message" placeholder="Message for me..." required/>
-          <button type="submit"><img src={rocketsend} alt="" /></button>
+            <input
+              type="text"
+              name="user_email"
+              placeholder="Your Email"
+              required
+            />
+            <input
+              type="text"
+              name="user_name"
+              placeholder="Your Name"
+              required
+            />
+            <textarea
+              type="text"
+              name="message"
+              placeholder="Message for me..."
+              required
+            />
+            <button type="submit">
+              <img src={rocketsend} alt="" />
+            </button>
           </form>
         </div>
       </div>
@@ -172,17 +234,15 @@ const SkillCard = (props) => {
 };
 
 const ProjectCard = (props) => {
-  const handleProjectClick = () =>{
+  const handleProjectClick = () => {
     window.open(props.link);
-  }
+  };
   return (
     <div className="project-card cardhover" onClick={handleProjectClick}>
       <div>
         <img src={props.img} alt="" />
         <h2>{props.name}</h2>
-        <p>
-          {props.detail}
-        </p>
+        <p>{props.detail}</p>
         <p>
           <span className="green">#reactjs </span>
           <span className="blue">#mongodb </span>
@@ -193,16 +253,15 @@ const ProjectCard = (props) => {
   );
 };
 
-
 const QualiCard = (props) => {
   return (
     <div className="qualicard">
       <h1>{props.type}</h1>
-      <div style={{height:"150px"}}>
-      <img src={props.image} width={100 + "px"} alt={props.type} />
+      <div style={{ height: "150px" }}>
+        <img src={props.image} width={100 + "px"} alt={props.type} />
       </div>
       <p>{props.detail}</p>
     </div>
-  )
-}
+  );
+};
 export default Home;
